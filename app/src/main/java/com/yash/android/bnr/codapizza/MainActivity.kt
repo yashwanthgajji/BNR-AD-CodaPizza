d8689875509173fba630d18390d701fa73127173
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-06-16 00:15:32 -0400

    initiating jetpack compose

--- app/src/main/java/com/yash/android/bnr/codapizza/MainActivity.kt
+++ app/src/main/java/com/yash/android/bnr/codapizza/MainActivity.kt
@@ -9,6 +9,5 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
