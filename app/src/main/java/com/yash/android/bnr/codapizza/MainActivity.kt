ac1cbacf4462c28dc1bf97501e5b9aa4b8e905a0
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-06-16 00:25:23 -0400

    hello compose code

--- app/src/main/java/com/yash/android/bnr/codapizza/MainActivity.kt
+++ app/src/main/java/com/yash/android/bnr/codapizza/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.yash.android.bnr.codapizza
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.material.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            Text(text = "Hello Compose!")
+        }
     }
 }
