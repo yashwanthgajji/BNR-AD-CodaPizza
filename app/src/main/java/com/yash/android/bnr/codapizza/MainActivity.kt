0f311cf660940b7b0d13190971b5c61cd25c4ee8
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-06-16 16:13:52 -0400

    pizza builder screen

--- app/src/main/java/com/yash/android/bnr/codapizza/MainActivity.kt
+++ app/src/main/java/com/yash/android/bnr/codapizza/MainActivity.kt
@@ -5,15 +5,14 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import com.yash.android.bnr.codapizza.model.Topping
 import com.yash.android.bnr.codapizza.model.ToppingPlacement
+import com.yash.android.bnr.codapizza.ui.PizzaBuilderScreen
 import com.yash.android.bnr.codapizza.ui.ToppingCell
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ToppingCell(topping = Topping.Olive, placement = ToppingPlacement.All) {
-
-            }
+            PizzaBuilderScreen()
         }
     }
 }
