b0bb21f23f03937ed7d8e253f863916d9ef9c7e1
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-06-16 23:43:15 -0400

    adding pizza size

--- app/src/main/java/com/yash/android/bnr/codapizza/model/Pizza.kt
+++ app/src/main/java/com/yash/android/bnr/codapizza/model/Pizza.kt
@@ -4,15 +4,24 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class Pizza(val toppings: Map<Topping, ToppingPlacement> = emptyMap()) : Parcelable {
+data class Pizza(
+    val size: PizzaSize = PizzaSize.Small,
+    val toppings: Map<Topping, ToppingPlacement> = emptyMap()
+) : Parcelable {
     val price: Double
-        get() = 9.99 + toppings.asSequence()
+        get() = when(size) {
+            PizzaSize.Small -> 7.99
+            PizzaSize.Medium -> 9.99
+            PizzaSize.Large -> 11.99
+            PizzaSize.ExtraLarge -> 13.99
+        } + toppings.asSequence()
             .sumOf { (_, toppingPlacement) ->
                 when (toppingPlacement) {
                     ToppingPlacement.Left, ToppingPlacement.Right -> 0.5
                     ToppingPlacement.All -> 1.0
                 }
             }
+
     fun withTopping(topping: Topping, toppingPlacement: ToppingPlacement?): Pizza {
         return copy(
             toppings = if (toppingPlacement == null) {
@@ -21,5 +30,9 @@ data class Pizza(val toppings: Map<Topping, ToppingPlacement> = emptyMap()) : Pa
                 toppings + (topping to toppingPlacement)
             }
         )
+    }
+
+    fun withSize(size: PizzaSize): Pizza {
+        return copy(size = size)
     }
 }
