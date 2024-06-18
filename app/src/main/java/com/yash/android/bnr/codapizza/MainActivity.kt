7e3e526f8972a4f667f62d6528e12208dcdd359f
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-06-17 22:22:29 -0400

    pizza preview on screen

--- app/src/main/java/com/yash/android/bnr/codapizza/MainActivity.kt
+++ app/src/main/java/com/yash/android/bnr/codapizza/MainActivity.kt
@@ -3,13 +3,16 @@ package com.yash.android.bnr.codapizza
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import com.yash.android.bnr.codapizza.ui.AppTheme
 import com.yash.android.bnr.codapizza.ui.PizzaBuilderScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PizzaBuilderScreen()
+            AppTheme {
+                PizzaBuilderScreen()
+            }
         }
     }
 }
