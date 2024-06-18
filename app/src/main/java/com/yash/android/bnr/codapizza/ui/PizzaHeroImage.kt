08d171692303bbca34174472b21e9252a2e0d3b3
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-06-17 21:58:20 -0400

    aligning pizza images with topping placement

--- app/src/main/java/com/yash/android/bnr/codapizza/ui/PizzaHeroImage.kt
+++ app/src/main/java/com/yash/android/bnr/codapizza/ui/PizzaHeroImage.kt
@@ -3,8 +3,12 @@ package com.yash.android.bnr.codapizza.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,16 +38,32 @@ fun PizzaHeroImage(
     pizza: Pizza,
     modifier: Modifier = Modifier
 ) {
-    Box(modifier = modifier) {
+    Box(modifier = modifier.aspectRatio(1f)) {
         Image(
             painter = painterResource(id = R.drawable.pizza_crust),
-            contentDescription = stringResource(id = R.string.pizza_preview)
+            contentDescription = stringResource(id = R.string.pizza_preview),
+            modifier = Modifier.fillMaxSize()
         )
         pizza.toppings.forEach { (topping, placement) ->
             Image(
                 painter = painterResource(id = topping.pizzaOverlayImage),
                 contentDescription = null,
-                modifier = Modifier.focusable(false)
+                contentScale = ContentScale.Crop,
+                alignment = when(placement) {
+                    ToppingPlacement.Left -> Alignment.TopStart
+                    ToppingPlacement.Right -> Alignment.TopEnd
+                    ToppingPlacement.All -> Alignment.Center },
+                modifier = Modifier
+                    .focusable(false)
+                    .aspectRatio(when(placement) {
+                        ToppingPlacement.Left, ToppingPlacement.Right -> 0.5f
+                        ToppingPlacement.All -> 1f
+                    })
+                    .align(when(placement) {
+                        ToppingPlacement.Left -> Alignment.CenterStart
+                        ToppingPlacement.Right -> Alignment.CenterEnd
+                        ToppingPlacement.All -> Alignment.Center
+                    })
             )
         }
     }
